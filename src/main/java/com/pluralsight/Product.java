56a575829b77506dc37c461ece652a2793b6d57c
mili Arsema <meleymm88@gmail.com>
2026-04-23 14:16:41 -0500

    Creates objects for ArrayList

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
-public class SearchInventory {
+public class Product {
+    //attributes/(data) fields/ data members
+    private int id;
+    private String name;
+    private float price;
+
+    //constructor (parameters)
+    public Product(int id, String name, float price) {
+        //attributes <~ parameters <~ arguments
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    //getters
+    public int getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public float getPrice() {
+        return this.price;
+    }
+
 }
